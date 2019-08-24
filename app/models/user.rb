75b7526f30fbe564e8bdd94f41312819819c3cab
ozroro <r.sleepyhead@gmail.com>
2019-08-24 21:35:43 +0900

    10.1.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
 
   has_secure_password
-  validates :password, length: {minimum: 6},presence: true
+  validates :password, presence: true, length: {minimum: 6}
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
