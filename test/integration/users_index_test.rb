a49e7942f352ef3d92191ebff9429abcd054e20c
ozroro <r.sleepyhead@gmail.com>
2019-09-03 13:30:16 +0900

    11.3

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,8 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
     @admin     = users(:michael)
     @non_admin = users(:archer)
+    @activated_user = users(:archer)
+    @non_activated_user = users(:tim)
   end
 
   test "should get the index page and links when logged in" do
@@ -29,7 +31,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    User.paginate(page: 1).each do |user|
+    User.where(activated: true).paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
   end
@@ -39,7 +41,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
@@ -55,6 +57,21 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "index not including non-activated user" do
+    log_in_as(@activated_user)
+    get users_path
+    # assert_select 'a[href=?]', user_path(@activated_user), text: @activated_user.name
+    assert_select 'a[href=?]', user_path(@non_activated_user), text: @non_activated_user.name, count: 0
+  end
+
+  test "should redirect to root_url when get non-activated user page path " do
+    log_in_as(@activated_user)
+    # get user_path(@user)
+    # assert_template 'users/show'
+    get user_path(@non_activated_user)
+    assert_redirected_to root_url
   end
   # test "the truth" do
   #   assert true
